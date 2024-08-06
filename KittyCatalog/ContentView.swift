914d0d5ad98afff2682d4645401a1695b8529bc1
David Lourenço <david.alexandre.laneiro.lourenco@gmail.com>
2024-08-06 12:54:59 +0100

    Initial Commit

---
+++ KittyCatalog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KittyCatalog
+//
+//  Created by David Lourenço on 06/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KittyCatalog/KittyCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  KittyCatalogApp.swift
+//  KittyCatalog
+//
+//  Created by David Lourenço on 06/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct KittyCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
