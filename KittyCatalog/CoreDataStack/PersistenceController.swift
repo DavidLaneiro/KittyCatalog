fb92282f66603b07118c29416500fa59c7bd4647
David Lourenço <david.alexandre.laneiro.lourenco@gmail.com>
2024-08-07 12:01:27 +0100

    Add PersistenceController implementation to CoreDataStack.

--- KittyCatalog/CoreDataStack/PersistenceController.swift
+++ KittyCatalog/CoreDataStack/PersistenceController.swift
@@ -5,4 +5,28 @@
 //  Created by David Lourenço on 07/08/2024.
 //
 
-import Foundation
+import CoreData
+
+class PersistenceController{
+    
+    static let shared = PersistenceController()
+
+    let container: NSPersistentContainer
+
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "CatBreedsModel")
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores { (description, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+    }
+
+    var viewContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+    
+}
